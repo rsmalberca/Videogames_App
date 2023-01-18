2bb174e3b16afdb4e0340d9f8c128f3c78dbd83d
rsmalberca <renzo.smalberca@gmail.com>
2023-01-18 13:36:31 -0500

    deploy

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import Home from "./components/Home/Home";
 import VideogameDetail from "./components/VideogameDetail/VideogameDetail";
 import NewVideogame from "./components/NewVideogame/NewVideogame";
 import axios from "axios";
-axios.defaults.baseURL = "http://localhost:3001/";
+axios.defaults.baseURL = "https://videogamespi-production-2949.up.railway.app";
 
 function App() {
   return (
